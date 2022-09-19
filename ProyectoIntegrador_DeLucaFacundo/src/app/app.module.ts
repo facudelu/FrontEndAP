b4bfe6e3761deafbec87408ac3874a61192d9ae9
facudelu <delucafacundo@gmail.com>
2022-09-19 16:48:44 -0300

    Primer avance con acerca de

--- ProyectoIntegrador_DeLucaFacundo/src/app/app.module.ts
+++ ProyectoIntegrador_DeLucaFacundo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { AcercaDeComponent } from './Components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
