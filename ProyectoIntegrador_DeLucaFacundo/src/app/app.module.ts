80f50d71d27af1d2f93caeaf0f584059adc7e143
facudelu <delucafacundo@gmail.com>
2022-09-19 18:08:23 -0300

    Educacion y experiencia

--- ProyectoIntegrador_DeLucaFacundo/src/app/app.module.ts
+++ ProyectoIntegrador_DeLucaFacundo/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { AcercaDeComponent } from './Components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './Components/experiencia/experiencia.component';
+import { EducacionComponent } from './Components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
