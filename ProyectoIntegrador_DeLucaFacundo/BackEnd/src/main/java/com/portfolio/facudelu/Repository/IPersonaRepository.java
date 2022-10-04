db95b85dfea08b6e28de323cded415528c4e83de
facudelu <delucafacundo@gmail.com>
2022-10-04 20:07:38 -0300

    Login y home

--- ProyectoIntegrador_DeLucaFacundo/BackEnd/src/main/java/com/portfolio/facudelu/Repository/IPersonaRepository.java
+++ ProyectoIntegrador_DeLucaFacundo/BackEnd/src/main/java/com/portfolio/facudelu/Repository/IPersonaRepository.java
@@ -2,6 +2,7 @@ package com.portfolio.facudelu.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.portfolio.facudelu.Entity.Persona;
 
 @Repository
--- ProyectoIntegrador_DeLucaFacundo/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_DeLucaFacundo/FrontEnd/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { HysSkillsComponent } from './Components/hys-skills/hys-skills.component
 import { ProyectosComponent } from './Components/proyectos/proyectos.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysSkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
